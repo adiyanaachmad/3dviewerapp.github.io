6c5ed2c5697c7b424a90c3b338c0470139934f62
adiyanaachmad <acreative3179@gmail.com>
2025-07-04 01:01:03 +0700

    Add_SoundEffect

--- js/button.js
+++ js/button.js
@@ -1,7 +1,19 @@
 document.querySelectorAll('.menu-btn').forEach(btn => {
   btn.addEventListener('click', () => {
-    btn.classList.remove('animate'); 
-    void btn.offsetWidth; 
-    btn.classList.add('animate'); 
+    btn.classList.remove('animate');
+    void btn.offsetWidth;
+    btn.classList.add('animate');
   });
+});
+
+const clickSound = new Audio('sounds/minecraft_click.mp3');
+
+function playSoundEffect() {
+  clickSound.currentTime = 0;
+  clickSound.play();
+}
+
+const soundElements = document.querySelectorAll('.sound-init');
+soundElements.forEach(element => {
+  element.addEventListener('click', playSoundEffect);
 });
